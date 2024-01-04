44523a72e86e259b5d4cf596e3cfa54adb8e98bd
Polina <poly.nika22@gmail.com>
2024-01-04 21:12:00 +0300

    commit 3

---
+++ lab1/App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Группа 224-371</Text>
+      <Text>Студентка Кузнецова П.А</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
